ab1ae651c981ea502f09066c5cc972e83a39e5f7
Ferntran <ferntran0214@gmail.com>
2020-08-06 00:19:47 +0700

    index-update

---
+++ templates/Greeny-Homepage/index.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-65px";
+    document.getElementById("menu").style.top = "0";
+  }
+}
